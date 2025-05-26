518e3f7bc5ec90f1640ad470a756273423b7003d
dmswl6310 <dmswl6310@gmail.com>
2025-05-26 16:03:26 +0900

    프로젝트 세팅 : PWA 최소기준을위한 meta데이터, robot.txt, 아이콘 파일 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,34 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
-      // 기타 옵션 설정 가능
-    })
+      registerType: "autoUpdate",
+      // workbox: {
+      //   globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      // },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "my PWA App",
+        short_name: "vitePWA",
+        description: "First vitePWA",
+        theme_color: "#FFDCDC",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
   ],
 });
