c65b1ecc96c7623c28ce1c028f79fbd0b12ec63c
dmswl6310 <dmswl6310@gmail.com>
2025-06-02 12:06:58 +0900

    feat: 프로젝트 기본 레이아웃 적용
    <header> 홈 버튼(로고), 검색 배너, components 버튼, about 버튼
    <body> 설명배너, 리스트 뷰
    <footer>이메일(링크), 이름(github링크)

---
+++ src/components/layout/AppFooter.tsx
@@ -0,0 +1,9 @@
+const AppFooter = () => {
+  return (
+    <div className="p-8">
+        footer입니다
+    </div>
+  );
+};
+
+export default AppFooter;
---
+++ src/components/layout/AppHeader.tsx
@@ -0,0 +1,9 @@
+const AppHeader = () => {
+  return (
+    <div className="p-8">
+        Header입니다
+    </div>
+  );
+};
+
+export default AppHeader;
