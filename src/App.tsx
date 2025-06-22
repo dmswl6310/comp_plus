4cdfc684ff5ca6918d5dc91b6d2ea1cf98620380
dmswl6310 <dmswl6310@gmail.com>
2025-06-22 23:50:38 +0900

    comment : 주석변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-
 import { Outlet } from "react-router-dom";
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
 
-const App=()=> {
-
+const App = () => {
+  // 중첩 라우팅을 위한 outlet
   return (
     <>
-    <Header/>
-     <main>
-      <Outlet/>
-     </main>
-     <Footer/>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
