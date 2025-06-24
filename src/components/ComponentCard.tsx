77a2416210d9e31a4c6b98f5dd4daf7ae7dd7cbc
dmswl6310 <dmswl6310@gmail.com>
2025-06-24 16:59:30 +0900

    style: 카드 gap 조정

--- src/components/ComponentCard.tsx
+++ src/components/ComponentCard.tsx
@@ -2,19 +2,21 @@ import { ComponentInfo } from "@/types/component.types";
 
 const ComponentCard = ({ info }: { info: ComponentInfo }) => {
   return (
-    <div className="flex aspect-[4/5] flex-col gap-4 rounded-lg bg-white p-6 shadow-sm transition transition-all transition-transform duration-300 ease-in-out hover:-translate-y-2 hover:shadow-md hover:ring hover:ring-gray-300">
+    <div className="flex aspect-[4/5] flex-col gap-4 bg-white p-6 shadow-sm transition transition-all transition-transform duration-300 ease-in-out hover:-translate-y-2 hover:rounded-lg hover:shadow-md hover:ring hover:ring-gray-300">
       <div className="aspect-[4/3] w-full overflow-hidden bg-gray-50">
         <img
           src={info.image}
           alt={`${info.name}`}
-          className="object-contiain h-full w-full p-4"
+          className="object-contiain h-full w-full"
         />
       </div>
-      <h3 className="font-bold text-gray-800">{info.name}</h3>
+      <h3 className="text-lg font-semibold text-gray-900">{info.name}</h3>
       {info.tags && (
-        <p className="text-xs text-gray-500 italic">{info.tags.join(", ")}</p>
+        <p className="-mt-2 text-xs text-gray-500 italic">
+          {info.tags.join(", ")}
+        </p>
       )}
-      <p className="text-gray-600">{info.description}</p>
+      <p className="text-sm text-gray-700">{info.description}</p>
     </div>
   );
 };
