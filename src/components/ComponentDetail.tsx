2b7c2d19e84b94c0282b86f07b9c5e8d61adcb5f
dmswl6310 <dmswl6310@gmail.com>
2025-07-09 17:32:19 +0900

    fix: 코드 하이라이터 테마 변경

--- src/components/ComponentDetail.tsx
+++ src/components/ComponentDetail.tsx
@@ -2,7 +2,7 @@ import PageTitle from "@/components/common/PageTitle";
 import { componentsData } from "@/data/componentsData";
 import { Navigate, useParams } from "react-router-dom";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-// import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { oneLight } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 const ComponentDetail = () => {
   const { id } = useParams<{ id: string }>();
@@ -32,19 +32,21 @@ const ComponentDetail = () => {
             {componentDetail.updatedAt.toDateString()}
           </p>
         </div>
-        <SyntaxHighlighter
-          language="tsx"
-          // style={oneDark}
-          wrapLines
-          wrapLongLines
-          customStyle={{
-            borderRadius: "8px",
-            padding: "1rem",
-            fontSize: "0.875rem",
-          }}
-        >
-          {componentDetail.code}
-        </SyntaxHighlighter>
+        <div>
+          <SyntaxHighlighter
+            language="tsx"
+            style={oneLight}
+            wrapLines
+            wrapLongLines
+            customStyle={{
+              borderRadius: "8px",
+              padding: "1rem",
+              fontSize: "0.875rem",
+            }}
+          >
+            {componentDetail.code}
+          </SyntaxHighlighter>
+        </div>
       </div>
     </div>
   );
