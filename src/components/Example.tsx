a4e0d957d008582364ef17543d092194f0f5bd2d
dmswl6310 <dmswl6310@gmail.com>
2025-08-11 17:22:10 +0900

    feat: 사용자가 component props바꿀수있는 기능추가

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,4 +1,5 @@
 import { componentMap, ComponentTypeMap } from "@/data/componentsData";
+import React from "react";
 import { useState } from "react";
 
 type ExampleProps<K extends keyof ComponentTypeMap> = {
@@ -14,18 +15,33 @@ const Example = <K extends keyof ComponentTypeMap>({
     ComponentTypeMap[K]
   >;
   const [isExecuted, setIsExecuted] = useState(false);
+  const [propsState, setPropsState] = useState(componentProps);
+  const handleChange = (key: string, newValue: string) => {
+    setPropsState((prev) => ({
+      ...prev,
+      [key]: newValue,
+    }));
+  };
 
   return (
     <div className="preview">
       <div className="example-code">
-        {`<${Component.name} ${Object.entries(componentProps)
-          .map(([key, value]) => `${key}="${value}"`)
-          .join(" ")} />`}
+        <span>{`<${Component.name} `}</span>
+        {Object.entries(propsState).map(([key, val]) => (
+          <span key={key}>
+            {key}=
+            <input
+              value={String(val)}
+              onChange={(e) => handleChange(key, e.target.value)}
+            />
+          </span>
+        ))}
+        {`/>`}
       </div>
       <button onClick={() => setIsExecuted(true)}>버튼</button>
       {isExecuted ? (
         <div className="rendered-result">
-          <Component {...componentProps} />
+          <Component {...propsState} />
         </div>
       ) : null}
     </div>
